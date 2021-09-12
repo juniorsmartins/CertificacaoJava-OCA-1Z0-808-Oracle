13aa587be92d9408a539e8dfed3251147e84d72f
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-09-12 16:09:50 -0400

    Preparação para certificação OCA da Oracle

---
+++ src/main/java/com/oca/string/Metodo_replace.java
@@ -0,0 +1,19 @@
+package com.oca.string;
+
+public class Metodo_replace 
+{
+    public static void main(String[] args) // Uso de chaves ao estilo Allman
+    {
+        // Método Replace - substitui caracteres da string original por novos caracteres e retorna a nova string
+        String messageOrigin = "Johnny Cash fez sucesso com a música, Hurt";
+        // String replace(char oldChar, char newChar) 
+        String messageNew = messageOrigin.replace('o', 'x');
+        // Letras O foram substituídas por x
+        System.out.println(messageNew);
+        
+        // Exemplo 2: o método replace possui duas declarações. A segunda é String replace(CharSequence target, CharSequence replacement)
+        String messageNew2 = messageOrigin.replace("a música", "o lendário som");
+        
+        System.out.println(messageNew2);
+    }
+}
---
+++ src/main/java/com/oca/string/Metodo_startsWith.java
@@ -0,0 +1,20 @@
+package com.oca.string;
+
+public class Metodo_startsWith 
+{
+    public static void main(String[] args) 
+    {
+        // Método startsWith - verifica se o prefixo de uma string corresponde com uma string fornecida e retorn boolean como resultado
+        String message = "SQL é uma linguagem de programação de paradigma declarativo das mais conhecida no mundo";
+        String inicio = "SQL";
+        String inicio2 = "linguagem";
+        // public boolean startsWith(String prefix)
+        boolean resultado = message.startsWith(inicio);
+        if(resultado == true)
+        {System.out.println("Verdadeiro! A string inicia com " + inicio);}
+        else {System.out.println("Falso! A string não inicia com " + inicio);}
+        // O método startsWith possui duas declarações. A segunda é: public boolean startsWith(String prefix, int toffset)
+        boolean resultado2 = message.startsWith(inicio2, 10);
+        System.out.println("Resultado2: " + resultado2 + "\n");
+    }
+}
