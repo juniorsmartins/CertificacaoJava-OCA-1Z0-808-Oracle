0d9937df741560d3ff9ed7cbeb0e8a12eb2e3a6d
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-09-12 14:36:28 -0400

    Preparação para certificação OCA da Oracle

--- src/main/java/com/oca/string/Metodo_charAt.java
+++ src/main/java/com/oca/string/Metodo_charAt.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.oca.string;
 
-/**
- *
- * @author junio
- */
-public class Metodo_charAt {
-    
+public class Metodo_charAt 
+{ 
+    public static void main(String[] args) // Estilo Allman de chaves
+    {
+        // Como utilizar o método de String chamado charAt
+        String message = " Deep Purple é uma banda super quente";
+        char c1, c2, c3, c4;     
+
+        // O método charAt da classe String retorna um valor char primitivo a partir do valor de índice int relacionado ao objeto string referenciado   
+        c1 = message.charAt(1);
+        c2 = message.charAt(6); 
+        c3 = message.charAt(13);
+        c4 = message.charAt(25);
+       System.out.println("Letras recuperadas por charAt: " + c1 + c2 + " " + c3 + " " + c4 + "uper");
+    }
 }
