1103b27baeae90959f4e6e911786ebe003c933fd
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-09-12 15:28:30 -0400

    Preparação para certificação OCA da Oracle

---
+++ src/main/java/com/oca/string/Metodo_concat.java
@@ -0,0 +1,21 @@
+package com.oca.string;
+
+public class Metodo_concat 
+{
+    public static void main(String[] args) 
+    {
+        // Metodo concat da classe String. Ele adiciona uma string ao final da string original
+        // String original
+        String message = "Scorpions é uma banda irada! ";
+        // O concat irá acrescentar uma string ao final da original
+        String messageConcat = message.concat("Cool");
+        
+        System.out.println("\n" + messageConcat);
+        
+        // Exemplo 2
+        String mensagem = "Cool";
+        String message2 = "Led Zeppeling é a maior banda de todos os tempos! ";
+        String messageConcat2 = message2.concat(mensagem);
+        System.out.println("\n" + messageConcat2 + "\n");
+    }
+}
