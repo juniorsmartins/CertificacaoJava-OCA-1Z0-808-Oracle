2e0440035b48bb69076f1c74ad37c961f80b1630
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-09-13 05:40:56 -0400

    Preparação para certificação OCA da Oracle

---
+++ src/main/java/com/oca/string/Metodo_trim.java
@@ -0,0 +1,15 @@
+package com.oca.string;
+
+public class Metodo_trim 
+{
+    public static void main(String[] args) 
+    {
+        // Metodo Trim - retorna a string inteira menos os caracteres de espaço em branco iniciais e finais do objeto string referenciado
+        // A única declaração do método é: public String trim()
+        String message, resultadoTrim; 
+        message = " O MariaDB é um Sistema Gerenciador de Banco de Dados Relacional (SGBDR) fork do MySQL ";
+        resultadoTrim = message.trim();
+        
+        System.out.println("Resultado: " + resultadoTrim);
+    }
+}
