d14330ede0da023399c390a809b189a116bc58e1
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-09-14 06:36:37 -0400

    Preparação para certificação OCA da Oracle

---
+++ src/main/java/com/oca/stringbuilder/Metodo_delete.java
@@ -0,0 +1,13 @@
+package com.oca.stringbuilder;
+
+public class Metodo_delete 
+{
+    public static void main(String[] args) 
+    {
+        StringBuilder sb = new StringBuilder("Estrela é um tipo de arquitetura de DataWarehouse.");
+        // O método delete remove caracteres de uma substring do objeto StringBuilder
+        // Há uma única declaração:
+        // public StringBuilder delete(int start, int end)
+        System.out.println(sb.delete(9, 20));
+    }
+}
