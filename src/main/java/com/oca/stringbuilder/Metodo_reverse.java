3370b2dda85cddae64fc07196dc6b0d579a28b42
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-09-15 07:16:45 -0400

    Estudo de Java

---
+++ src/main/java/com/oca/stringbuilder/Metodo_reverse.java
@@ -0,0 +1,13 @@
+package com.oca.stringbuilder;
+
+public class Metodo_reverse 
+{
+    public static void main(String[] args) 
+    {
+        // O método reverse inverte a ordem da sequência de caracteres
+        // Há única declaração: public StringBuilder reverse ()
+        StringBuilder palavras = new StringBuilder("Alura Multiverso");
+        palavras = palavras.reverse();
+        System.out.println("Resultado do reverse: " + palavras);
+    }
+}
