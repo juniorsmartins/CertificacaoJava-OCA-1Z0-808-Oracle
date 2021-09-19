cb8a4be3225ad733af0412ce54f6f48ff358bee3
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-09-19 13:21:28 -0400

    Estudo de Java

--- src/main/java/com/oca/alura/importar/Importar.java
+++ src/main/java/com/oca/alura/importar/Importar.java
@@ -1,5 +1,15 @@
 package com.oca.alura.importar;
 
-public class Importar {
-    
+import com.oca.alura.importar.Endereco; // É opcional importar classes do mesmo pacote. Não precisa, mas compila e roda.
+
+public class Importar 
+{
+    public static void main(String[] args) 
+    {
+        Endereco endereco = new Endereco();
+        endereco.rua = "Rua Cornélio Procópio";
+        endereco.numero = "1500";
+        endereco.bairro = "Porto";
+        System.out.println(": " + endereco.rua + " : " + endereco.numero + " : ".concat(endereco.bairro));
+    }
 }
